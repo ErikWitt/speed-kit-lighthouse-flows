b7a2adabfdf6dca439d642883ab19269a7ea1b9a
Erik Witt <ew@baqend.com>
2022-05-05 08:17:25 +0200

    Change flow to decathlon

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import open from 'open';
 import puppeteer from 'puppeteer';
 import {startFlow} from 'lighthouse/lighthouse-core/fraggle-rock/api.js';
 
-const start = 'https://www.engelhorn.de/'
-const home = 'https://www.engelhorn.de/de-de/damen/';
-const pdp = 'https://www.engelhorn.de/de-de/raffaello-rossi-damen-joggerpants-candy-less-pipe-blau-V1075013E.html';
-const plp = 'https://www.engelhorn.de/de-de/damen/marken/r/raffaello-rossi/';
+const start = 'https://www.decathlon.de/'
+const cat = 'https://www.decathlon.de/browse/c0-damen/c1-bademode/c2-neoprenbekleidung/_/N-6t5h92';
+const pdp = 'https://www.decathlon.de/p/trekkingshorts-trek-500-damen/_/R-p-306980?mc=8544767&c=GR%C3%9CN_KHAKI';
+const plp = 'https://www.decathlon.de/special-page/_/N-x6qfruZzB50HB';
 
 async function captureReport(testId, group) {
   const browser = await puppeteer.launch({
@@ -24,18 +24,18 @@ async function captureReport(testId, group) {
   await page.setCookie({
     name: 'baqend-speedkit-ab-test-info',
     value: `%7B%22group%22%3A%22${group}%22%2C%22testId%22%3A%22${testId}%22%7D`,
-    domain: 'www.engelhorn.de'
+    domain: 'www.decathlon.de'
   });
   // Go to start page and accept cookies
   await page.goto(start, {waitUntil: 'load'});
-  const confirmationSelector = '#cmpwelcomebtnyes .cmpboxbtn';
+  const confirmationSelector = '#didomi-notice-agree-button';
   await page.waitForSelector(confirmationSelector);
   await page.click(confirmationSelector);
 
   // Run lighthouse tests
   const flow = await startFlow(page, { name: 'Engelhorn comparisson' });
-  await navigate(flow, home, 'home page');
-  await navigate(flow, plp, 'listing page');
+  await navigate(flow, cat, 'category page');
+  await navigate(flow, plp, 'sales page');
   await navigate(flow, pdp, 'product page');
 
   // Generate reports
@@ -55,8 +55,8 @@ async function navigate(flow, page, name) {
       settingsOverrides: {
         disableStorageReset: true,
         maxWaitForLoad: 10000,
-        screenEmulation: { disabled: true },
-        formFactor: 'mobile',
+        //screenEmulation: { disabled: true },
+        //formFactor: 'mobile',
       },
     },
   });
